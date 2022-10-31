d5ad8f2219217ef76c1cf4936f2b891daf83a8ed
颯己 市川 <200499@jc-21.jp>
2022-10-31 15:39:58 +0900

    Sentimentをアプリケーションで動かす修正

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
@@ -1,6 +1,7 @@
 package jp.jc21.t.yoshizawa.WEB01;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,9 +14,9 @@ public class Sentiment {
 	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 		Senti message = getSentiment("めっちゃ楽しい！！");
 		if (message != null) {
-			System.out.println(message.documents[0].confidenceScores.negative);
-			System.out.println(message.documents[0].confidenceScores.neutral);
-			System.out.println(message.documents[0].confidenceScores.positive);
+			System.out.println("negative : " + message.documents[0].confidenceScores.negative);
+			System.out.println("neutral : " + message.documents[0].confidenceScores.neutral);
+			System.out.println("positive : " + message.documents[0].confidenceScores.positive);
 		}
 	}
 
@@ -36,10 +37,10 @@ public class Sentiment {
 
 		String jsonData = new Gson().toJson(src);
 
-		//InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
+		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
 
-		//JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
-		JsonReader reader = WebApiConnector.postJsonReader(url,map,jsonData);
+		JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
+		//JsonReader reader = WebApiConnector.postJsonReader(url,map,jsonData);
 		Senti message = null;
 		if (reader != null) {
 			message = gson.fromJson(reader, Senti.class);
